8d6305aed0f627f08345220f69298b13f8061386
Romeo Folie <romeofolie1@gmail.com>
2020-07-18 09:50:52 +0000

    removed unnecessary readmes and added linkedin icon and links to all social media

--- components/Aside.vue
+++ components/Aside.vue
@@ -17,7 +17,7 @@
           </li>
           <li>
             <a class="nav-link" href="#projects" onclick="hideSideNav()"
-              >Works</a
+              >Work</a
             >
           </li>
           <li>
@@ -30,17 +30,25 @@
 
       <ul class="side-social-media-nav">
         <li>
-          <a href="#github">
+          <a target="_blank" href="https://github.com/romeo-folie">
             <img src="/github.svg" alt="github_icon" />
           </a>
         </li>
         <li>
-          <a href="#instagram">
+          <a
+            target="_blank"
+            href="https://linkedin.com/in/romeo-nutifafa-folie-464a25160"
+          >
+            <img src="/linkedin.svg" alt="linkedin_icon" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://instagram.com/romeo__nf">
             <img src="/instagram.svg" alt="instagram_icon" />
           </a>
         </li>
         <li>
-          <a href="#twitter">
+          <a target="_blank" href="https://twitter.com/rxnf_">
             <img src="/twitter.svg" alt="twitter_icon" />
           </a>
         </li>
--- components/LeftBar.vue
+++ components/LeftBar.vue
@@ -6,17 +6,25 @@
       data-aos-duration="2000"
     >
       <li>
-        <a href="#github">
+        <a target="_blank" href="https://github.com/romeo-folie">
           <img src="/github.svg" alt="github_icon" />
         </a>
       </li>
       <li>
-        <a href="#instagram">
+        <a
+          target="_blank"
+          href="https://linkedin.com/in/romeo-nutifafa-folie-464a25160"
+        >
+          <img src="/linkedin.svg" alt="linkedin_icon" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="https://instagram.com/romeo__nf">
           <img src="/instagram.svg" alt="instagram_icon" />
         </a>
       </li>
       <li>
-        <a href="#twitter">
+        <a target="_blank" href="https://twitter.com/rxnf_">
           <img src="/twitter.svg" alt="twitter_icon" />
         </a>
       </li>
